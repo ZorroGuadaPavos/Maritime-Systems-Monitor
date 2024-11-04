a19b29cdd2b61e8a3cf675f55fa2e1f219226b20
ZorroGuardaPavos <4ndres.or@gmail.com>
2024-11-04 14:04:04 +0100

    Rename auth test file and add access token creation test; enhance authentication coverage

--- backend/tests/auth/test_services.py
+++ backend/tests/auth/test_services.py
@@ -1,6 +1,8 @@
+from datetime import timedelta
+
 from sqlmodel import Session
 
-from src.auth.services import authenticate
+from src.auth.services import authenticate, create_access_token
 from src.users.schemas import UserCreate
 from src.users.services import create_user
 from tests.utils.utils import random_email, random_lower_string
@@ -21,3 +23,12 @@ def test_not_authenticate_user(db: Session) -> None:
     password = random_lower_string()
     user = authenticate(session=db, email=email, password=password)
     assert user is None
+
+
+def test_create_access_token(db: Session) -> None:
+    email = random_email()
+    password = random_lower_string()
+    user_in = UserCreate(email=email, password=password)
+    user = create_user(session=db, user_create=user_in)
+    token = create_access_token(subject=user.id, expires_delta=timedelta(minutes=15))
+    assert isinstance(token, str)
